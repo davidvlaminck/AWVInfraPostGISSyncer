8d967a4ebd11509dbad5fa6ab7de4c9323d19604
davidvlaminck <davidvlaminck85@gmail.com>
2022-08-11 12:22:39 +0200

    added NaamGewijzigdProcessor.py

--- EventProcessors/NaamGewijzigdProcessor.py
+++ EventProcessors/NaamGewijzigdProcessor.py
@@ -1,38 +1,66 @@
 import logging
+import time
 
-
-from EMInfraImporter import EMInfraImporter
-from EventProcessors.NieuwAssetProcessor import NieuwAssetProcessor
 from EventProcessors.SpecificEventProcessor import SpecificEventProcessor
 
 
 class NaamGewijzigdProcessor(SpecificEventProcessor):
-    def __init__(self, tx_context, em_infra_importer: EMInfraImporter):
-        super().__init__(tx_context, em_infra_importer)
+    def __init__(self, cursor, em_infra_importer):
+        super().__init__(cursor, em_infra_importer)
 
     def process(self, uuids: [str]):
-        raise NotImplementedError
-        assetDicts = self.em_infra_importer.import_assets_from_webservice_by_uuids(asset_uuids=uuids)
+        logging.info(f'started changing naam/naampad/parent')
+        start = time.time()
 
-        self.process_dicts(assetDicts)
+        asset_dicts = self.em_infra_importer.import_assets_from_webservice_by_uuids(asset_uuids=uuids)
+        values = self.create_values_string_from_dicts(assets_dicts=asset_dicts)
+        self.perform_update_with_values(cursor=self.cursor, values=values)
+        # TODO change parent uuid as well
 
-    def process_dicts(self, assetDicts):
-        logging.info(f'started changing naam/naampad/parent of {len(assetDicts)} assets')
-        for asset_dict in assetDicts:
-            korte_uri = asset_dict['@type'].split('/ns/')[1]
-            ns = korte_uri.split('#')[0]
-            assettype = korte_uri.split('#')[1]
-            if '-' in assettype:
-                assettype = '`' + assettype + '`'
-            naampad = None
+        end = time.time()
+        logging.info(f'updated {len(asset_dicts)} assets in {str(round(end - start, 2))} seconds.')
+
+    @staticmethod
+    def create_values_string_from_dicts(assets_dicts):
+        values = ''
+        for asset_dict in assets_dicts:
+            uuid = asset_dict['@id'].replace('https://data.awvvlaanderen.be/id/asset/', '')[0:36]
+
             naam = None
-            if 'NaampadObject.naampad' in asset_dict:
-                naampad = asset_dict['NaampadObject.naampad']
             if 'AIMNaamObject.naam' in asset_dict:
                 naam = asset_dict['AIMNaamObject.naam']
-            self.tx_context.run(f"MATCH (a:{ns}:{assettype} "
-                                "{uuid: $uuid}) SET a.naam = $naam, a.naampad = $naampad",
-                                uuid=asset_dict['AIMObject.assetId']['DtcIdentificator.identificator'][0:36],
-                                naam=naam,
-                                naampad=naampad)
-        logging.info('done')
+            elif 'AbstracteAanvullendeGeometrie.naam' in asset_dict:
+                naam = asset_dict['AIMNaamObject.naam']
+
+            naampad = None
+            if 'NaampadObject.naampad' in asset_dict:
+                naampad = asset_dict['NaampadObject.naampad']
+
+            values += f"('{uuid}',"
+
+            if naam is None:
+                values += 'NULL'
+            else:
+                naam = naam.replace("'", "''")
+                values += f"'{naam}'"
+
+            if naampad is None:
+                values += ',NULL'
+            else:
+                naampad = naampad.replace("'", "''")
+                values += f",'{naampad}'"
+            values = values + '),'
+        return values
+
+    @staticmethod
+    def perform_update_with_values(cursor, values):
+        update_query = f"""
+        WITH s (uuid, naam, naampad)  
+            AS (VALUES {values[:-1]}),
+        to_update AS (
+            SELECT uuid::uuid AS uuid, naam, naampad FROM s)
+        UPDATE assets 
+        SET naam = to_update.naam, naampad = to_update.naampad
+        FROM to_update 
+        WHERE to_update.uuid = assets.uuid;"""
+        cursor.execute(update_query)
