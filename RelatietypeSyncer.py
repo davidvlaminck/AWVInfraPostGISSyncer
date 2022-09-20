be90886dde8fc3b4470bba4044f57cdf98c50c2a
davidvlaminck <davidvlaminck85@gmail.com>
2022-09-20 22:29:38 +0200

    added uri property

--- RelatietypeSyncer.py
+++ RelatietypeSyncer.py
@@ -26,9 +26,10 @@ class RelatietypeSyncer:
             definitie = relatietype_dict.get('definitie', '').replace("'", "''")
             actief = relatietype_dict['actief']
             gericht = relatietype_dict['gericht']
+            uri = relatietype_dict.get('uri', '')
 
             values += f"('{uuid}','{naam}',"
-            null_values = [label, definitie]
+            null_values = [uri, label, definitie]
             for null_value in null_values:
                 if null_value != '':
                     values += f"'{null_value}',"
@@ -37,25 +38,25 @@ class RelatietypeSyncer:
             values += f"{actief},{gericht}),"
 
         insert_query = f"""
-WITH s (uuid, naam, label, definitie, actief, gericht) 
+WITH s (uuid, naam, uri, label, definitie, actief, gericht) 
     AS (VALUES {values[:-1]}),
 t AS (
-    SELECT uuid::uuid AS uuid, naam, label, definitie, actief, gericht
+    SELECT uuid::uuid AS uuid, naam, uri, label, definitie, actief, gericht
     FROM s),
 to_insert AS (
     SELECT t.* 
     FROM t
         LEFT JOIN public.relatietypes ON relatietypes.uuid = t.uuid 
     WHERE relatietypes.uuid IS NULL)
-INSERT INTO public.relatietypes (uuid, naam, label, definitie, actief, gericht)
-SELECT to_insert.uuid, to_insert.naam, to_insert.label, to_insert.definitie, to_insert.actief, to_insert.gericht
+INSERT INTO public.relatietypes (uuid, naam, uri, label, definitie, actief, gericht)
+SELECT to_insert.uuid, to_insert.naam, to_insert.uri, to_insert.label, to_insert.definitie, to_insert.actief, to_insert.gericht
 FROM to_insert;"""
 
         update_query = f"""
-WITH s (uuid, naam, label, definitie, actief, gericht)
+WITH s (uuid, naam, uri, label, definitie, actief, gericht)
     AS (VALUES {values[:-1]}),
 t AS (
-    SELECT uuid::uuid AS uuid, naam, label, definitie, actief, gericht
+    SELECT uuid::uuid AS uuid, naam, uri, label, definitie, actief, gericht
     FROM s),
 to_update AS (
     SELECT t.* 
@@ -63,7 +64,7 @@ to_update AS (
         LEFT JOIN public.relatietypes ON relatietypes.uuid = t.uuid 
     WHERE relatietypes.uuid IS NOT NULL)
 UPDATE public.relatietypes 
-SET naam = to_update.naam, label = to_update.label, definitie = to_update.definitie, actief = to_update.actief, 
+SET naam = to_update.naam, uri = to_update.uri, label = to_update.label, definitie = to_update.definitie, actief = to_update.actief, 
     gericht = to_update.gericht
 FROM to_update 
 WHERE to_update.uuid = relatietypes.uuid;"""
