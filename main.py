1e0e29f6f4c048ea24ce3d82858cbae126cee48a
Arthur <devosarthur@gmail.com>
2022-12-08 12:30:55 +0100

    Update

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ from Syncer import Syncer
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
-    settings_manager = SettingsManager(settings_path='.\\settings_postgiscreator.json')
+    settings_manager = SettingsManager(
+        settings_path='.\\settings_postgiscreator.json')
     db_settings = settings_manager.settings['databases']['prd']
 
-    connector = PostGISConnector(**db_settings)
+    connector = PostGISConnector(host=db_settings['host'], port=db_settings['port'],
+                                 user=db_settings['user'], password=db_settings['password'],
+                                 database=db_settings['database'])
 
     requester = RequesterFactory.create_requester(settings=settings_manager.settings, auth_type='JWT', env='prd')
     request_handler = RequestHandler(requester)
 
     eminfra_importer = EMInfraImporter(request_handler)
-    syncer = Syncer(connector=connector, request_handler=request_handler, eminfra_importer=eminfra_importer,
-                    settings=settings_manager.settings)
+    syncer = Syncer(connector=connector, request_handler=request_handler, eminfra_importer=eminfra_importer, settings=settings_manager.settings)
 
-    # syncer.start_syncing(stop_when_fully_synced=True)
     syncer.start_syncing()
+
     # set up database users
     # install postgis: CREATE EXTENSION postgis;
