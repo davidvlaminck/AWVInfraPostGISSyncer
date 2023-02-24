e744bdcdd8918ffb34cc74dd591140cb5e7c4706
davidvlaminck <david.vlaminck@mow.vlaanderen.be>
2023-02-24 12:35:32 +0100

    removed comments

--- EventProcessors/AssetProcessors/BestekGewijzigdProcessor.py
+++ EventProcessors/AssetProcessors/BestekGewijzigdProcessor.py
@@ -24,9 +24,5 @@ class BestekGewijzigdProcessor(SpecificEventProcessor):
         BestekKoppelingSyncer.update_bestekkoppelingen_by_asset_uuids(connection=connection, asset_uuids=asset_uuid_list,
                                                                       bestek_koppelingen_dicts_list=koppelingen_list)
 
-        # asset_dicts = self.eminfra_importer.import_assets_from_webservice_by_uuids(asset_uuids=uuids)
-        # values = NieuwAssetProcessor.create_values_string_from_dicts(connection=connection, assets_dicts=asset_dicts)
-        # NieuwAssetProcessor.perform_insert_with_values(connection=connection, values=values)
-
         end = time.time()
         logging.info(f'updated bestekkoppelingen of {amount} asset(s) in {str(round(end - start, 2))} seconds.')
