492aeb1a47c30d318bab98b6a280035349a74681
VerdoodtDries <dries@nordend.eu>
2025-02-27 11:13:53 +0100

    Append helper function "construct_naampad()"

--- Helpers.py
+++ Helpers.py
@@ -28,3 +28,19 @@ def chunked(seq, chunksize):
     """Yields items from an iterator in list chunks."""
     for chunk in ichunked(seq, chunksize):
         yield list(chunk)
+
+def construct_naampad(input_dict: dict) -> str:
+    """
+    Construct naampad by walking recursively in a nested "parent" dictionary, searching for the "naam" key.
+    Concatenates all "naam" values, starting from the top of the nested tree.
+    :param input_dict:
+    :return: str
+    """
+    naam_list = []
+    while "naam" in input_dict:
+        naam_list.insert(0, input_dict["naam"]) # insert at first list index position
+        if "parent" in input_dict:
+            input_dict = input_dict["parent"]
+        else:
+            break
+    return "/".join(naam_list)  # concatenate naampad, using "/" as a separator character
